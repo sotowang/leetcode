1e09c243a6b5e612c81409fc3e2c817357ae6b47
sotowang <sotowang@qq.com>
2019-09-28 21:33:29 +0800

    字节跳动：String

---
+++ src/main/byteDance/string/t2/LongestCommonPrefix.java
@@ -0,0 +1,27 @@
+package byteDance.string.t2;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || c != strs[j].charAt(i) ) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestCommonPrefix().longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+        System.out.println(new LongestCommonPrefix().longestCommonPrefix(new String[]{"aa", "a", "car"}));
+        System.out.println(new LongestCommonPrefix().longestCommonPrefix(new String[]{"aa", "a"}));
+
+    }
+}
