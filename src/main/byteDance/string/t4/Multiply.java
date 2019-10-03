18cee8ef02cdb76207528ac04ab586fc5447c645
sotowang <sotowang@qq.com>
2019-10-03 21:48:34 +0800

    bytedance :string

--- src/main/byteDance/string/t4/Multiply.java
+++ src/main/byteDance/string/t4/Multiply.java
@@ -2,6 +2,33 @@ package byteDance.string.t4;
 
 public class Multiply {
     public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int res[] = new int[num1.length() + num2.length()];
 
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int m = num1.charAt(i) - '0';
+            for (int j = num2.length()-1; j >=0 ; j--) {
+                int n = num2.charAt(j) - '0';
+                int sum = res[i+j+1] +m * n;
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < res.length; i++) {
+            if (i == 0 && res[i] == 0) {
+                continue;
+            }
+            result.append(res[i]);
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Multiply().multiply("123", "456"));
+        System.out.println(new Multiply().multiply("99999", "0"));
     }
 }
---
+++ src/main/byteDance/string/t6/SimplifyPath.java
@@ -0,0 +1,14 @@
+package byteDance.string.t6;
+
+import java.util.Stack;
+
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SimplifyPath().simplifyPath("/a/./b/../../c/"));
+
+    }
+}
