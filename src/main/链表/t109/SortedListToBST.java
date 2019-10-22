6a52e2f662879f5b571c6829f72495555b96fbb0
sotowang <sotowang@qq.com>
2019-10-22 22:03:05 +0800

    链表

--- src/main/链表/t109/SortedListToBST.java
+++ src/main/链表/t109/SortedListToBST.java
@@ -10,20 +10,46 @@ import 链表.ListNode;
  */
 public class SortedListToBST {
     public TreeNode sortedListToBST(ListNode head) {
-
-    }
-
-    private ListNode findMiddle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return new TreeNode(head.val);
+        }
+        ListNode preSlow = new ListNode(Integer.MIN_VALUE);
+        preSlow.next = head;
         ListNode fast = head;
         ListNode slow = head;
-        while (fast != null ) {
-            if (fast.next == null || fast.next.next == null) {
-                break;
-            }
+        //找中间结点
+        while (fast != null && fast.next != null) {
             slow = slow.next;
+            preSlow = preSlow.next;
             fast = fast.next.next;
-
         }
+        //断开节点
+        preSlow.next = null;
+        //此时slow指向中间结点
+        TreeNode root = new TreeNode(slow.val);
+
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        ListNode l_10 = new ListNode(-10);
+        ListNode l_3 = new ListNode(-3);
+        ListNode l0 = new ListNode(0);
+        ListNode l5 = new ListNode(5);
+        ListNode l9 = new ListNode(9);
+
+        l_10.next = l_3;
+        l_3.next = l0;
+        l0.next = l5;
+        l5.next = l9;
+
+        TreeNode res = new SortedListToBST().sortedListToBST(l_10);
+
     }
 
 }
---
+++ src/main/链表/t138/CopyRandomList.java
@@ -0,0 +1,66 @@
+package 链表.t138;
+
+/**
+ * @auther: wangsongtao_sx
+ * @date: 2019/10/22 21:20
+ */
+public class CopyRandomList {
+    static class Node {
+        public int val;
+        public Node next;
+        public Node random;
+
+        public Node() {}
+
+        public Node(int _val,Node _next,Node _random) {
+            val = _val;
+            next = _next;
+            random = _random;
+        }
+    }
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node cur = head;
+        while (cur != null) {
+            //新建一个节点,节点next指向cur的next
+            Node cpNode = new Node(cur.val, cur.next, null);
+            cur.next = cpNode;
+            cur = cur.next.next;
+        }
+        cur = head;
+        Node cpNode = cur.next;
+        Node cphead = cur.next;
+        while (cur != null) {
+            cpNode.random = cur.random == null ? null : cur.random.next;
+            cur = cur.next.next;
+            if (cur == null) {
+                break;
+            }
+            cpNode.next = cur.next;
+            cpNode = cur.next;
+        }
+        return cphead;
+    }
+
+    public static void main(String[] args) {
+        Node l1 = new Node();
+        Node l2 = new Node();
+        l1.random = l2;
+        l1.val = 1;
+        l2.val = 2;
+        l1.next = l2;
+        l2.random = l2;
+        l2.next = null;
+
+        Node l_1 = new Node();
+        l_1.val = -1;
+//        Node res = new CopyRandomList().copyRandomList(l1);
+        Node res = new CopyRandomList().copyRandomList(l_1);
+
+    }
+
+
+}
