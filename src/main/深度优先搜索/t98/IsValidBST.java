b600d5226f8dc17f4f75224f2cbf4ae1c8c97503
sotowang <sotowang@qq.com>
2019-11-05 14:18:44 +0800

    readme toc

---
+++ src/main/深度优先搜索/t98/IsValidBST.java
@@ -0,0 +1,13 @@
+package 深度优先搜索.t98;
+
+import 链表.t109.TreeNode;
+
+/**
+ * @auther: sotowang
+ * @date: 2019/11/04 22:23
+ */
+public class IsValidBST {
+    public boolean isValidBST(TreeNode root) {
+
+    }
+}
