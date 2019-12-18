6bafe73f72c6d2a45f73d5351ae5611223f16453
sotowang <sotowang@qq.com>
2019-12-18 22:27:25 +0800

    其他

---
+++ src/main/动态规划/t338/CountBits.java
@@ -0,0 +1,30 @@
+package 动态规划.t338;
+
+import java.util.Arrays;
+
+/**
+ * @auther: sotowang
+ * @date: 2019/12/18 19:13
+ */
+
+
+public class CountBits  {
+    public int[] countBits(int num) {
+        int[] dp = new int[num + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= num; i++) {
+            dp[i] = dp[i & (i - 1)] + 1;
+        }
+        return dp;
+    }
+
+    public static void main(String[] args) {
+        CountBits countBits = new CountBits();
+        int[] res = countBits.countBits(5);
+        Arrays.stream(res).forEach(System.out::print );
+        System.out.println();
+        int[] res1 = countBits.countBits(2);
+        Arrays.stream(res1).forEach(System.out::print );
+
+    }
+}
