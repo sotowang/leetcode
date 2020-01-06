0cc11ecd486da98c4c2a630ee95779b0acb211e0
sotowang <sotowang@qq.com>
2020-01-06 22:21:43 +0800

    1162

--- src/main/广度优先搜索/t1162/MaxDistance.java
+++ src/main/广度优先搜索/t1162/MaxDistance.java
@@ -1,11 +1,29 @@
 package 广度优先搜索.t1162;
 
+import java.util.LinkedList;
+
 /**
  * @auther: sotowang
  * @date: 2020/01/06 21:02
  */
 public class MaxDistance {
     public int maxDistance(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
+            return -1;
+        }
+        LinkedList<int[]> sea = new LinkedList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0) {
+                    sea.offer(new int[]{i, j});
+                }
+            }
+        }
+        if (sea.size() == grid.length * grid[0].length || sea.size() == 0) {
+            return -1;
+        }
+        while (!sea.isEmpty()) {
 
+        }
     }
 }
