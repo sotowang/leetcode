e334faa0a5e0c08fc3e95e2d641e0ba9875b45a4
sotowang <sotowang@qq.com>
2020-01-07 21:20:41 +0800

    二分查找-1157

---
+++ src/main/二分查找/t1157/MajorityChecker.java
@@ -0,0 +1,15 @@
+package 二分查找.t1157;
+
+/**
+ * @auther: sotowang
+ * @date: 2020/01/07 15:32
+ */
+public class MajorityChecker {
+    public MajorityChecker(int[] arr) {
+
+    }
+
+    public int query(int left, int right, int threshold) {
+        return 0;
+    }
+}
