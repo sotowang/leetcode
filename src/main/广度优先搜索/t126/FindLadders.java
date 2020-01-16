624701159f15b45914dc4f2019085a2e51ac986a
sotowang <sotowang@qq.com>
2020-01-16 22:26:35 +0800

    广度优先搜索.t126

--- src/main/广度优先搜索/t126/FindLadders.java
+++ src/main/广度优先搜索/t126/FindLadders.java
@@ -1,5 +1,11 @@
 package 广度优先搜索.t126;
 
+import 二分查找.t668.FindKthNumber;
+
+import java.time.temporal.Temporal;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -8,7 +14,54 @@ import java.util.List;
  */
 public class FindLadders {
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        return null;
+        List<List<String>> res = new ArrayList<>();
+        if (wordList.contains(endWord)) {
+            List<String> usedWord = new ArrayList<>();
+            backTrace(beginWord, endWord, new ArrayList<>(wordList), new ArrayList<>(), res, usedWord);
+        }
+        return res;
+    }
 
+    private void backTrace(String beginWord,
+                           String endWord,
+                           ArrayList<String> dicts,
+                           ArrayList<String> tempList,
+                           List<List<String>> res,
+                           List<String> usedWords) {
+        tempList.add(beginWord);
+        if (beginWord.equals(endWord)) {
+            res.add(new ArrayList<>(tempList));
+            return;
+        }
+        for (int i = 0; i < beginWord.length(); i++) {
+            char[] beginChar = beginWord.toCharArray();
+            for (char j = 'a'; j <= 'z'; j++) {
+                char temp = beginChar[i];
+                beginChar[i] = j;
+                String tempStr = new String(beginChar);
+                if (dicts.contains(tempStr) && !tempList.contains(tempStr)) {
+                    dicts.remove(tempStr);
+                    backTrace(tempStr, endWord, dicts, tempList, res, usedWords);
+                    dicts.add(tempStr);
+                }
+                beginChar[i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String beginWord = "hit";
+        String endWord = "cog";
+        List<String> wordList = new ArrayList(){
+            {
+                add("hot");
+                add("dot");
+                add("dog");
+                add("lot");
+                add("log");
+                add("cog");
+            }
+        };
+        System.out.println(new FindLadders().findLadders(beginWord, endWord, wordList));
     }
 }
