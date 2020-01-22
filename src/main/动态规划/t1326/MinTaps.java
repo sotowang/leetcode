63cebfd0623d1e281eaef4f5b46f7d6bf280d48a
sotowang <sotowang@qq.com>
2020-01-22 23:23:01 +0800

    动态规划.t1326

---
+++ src/main/动态规划/t1326/MinTaps.java
@@ -0,0 +1,11 @@
+package 动态规划.t1326;
+
+/**
+ * @auther: sotowang
+ * @date: 2020/1/22 23:21
+ */
+public class MinTaps {
+    public int minTaps(int n, int[] ranges) {
+
+    }
+}
--- src/main/树/t1325/RemoveLeafNodes.java
+++ src/main/树/t1325/RemoveLeafNodes.java
@@ -8,6 +8,33 @@ import 广度优先搜索.TreeNode;
  */
 public class RemoveLeafNodes {
     public TreeNode removeLeafNodes(TreeNode root, int target) {
-
+        if (root == null) {
+            return null;
+        }
+        if (root.left == null && root.right == null && target == root.val) {
+            return null;
+        }
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+        if (root.left == null && root.right == null && target == root.val) {
+            return null;
+        }
+        return root;
     }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2_1 = new TreeNode(2);
+        TreeNode t2_2 = new TreeNode(2);
+        TreeNode t2_3 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        t1.left = t2_1;
+        t2_1.left = t2_2;
+        t1.right = t3;
+        t3.left = t2_3;
+        t3.right = t4;
+        TreeNode res = new RemoveLeafNodes().removeLeafNodes(t1, 2);
+    }
+
 }
