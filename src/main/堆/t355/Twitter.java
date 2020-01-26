508ce3a67f592f0b4ed7a1693541ac3d98a2827b
sotowang <sotowang@qq.com>
2020-01-26 22:55:20 +0800

    堆.t355

---
+++ src/main/堆/t355/Twitter.java
@@ -0,0 +1,35 @@
+package 堆.t355;
+
+import java.util.List;
+
+/**
+ * @auther: sotowang
+ * @date: 2020/1/26 21:41
+ */
+public class Twitter {
+    private int time;
+    /** Initialize your data structure here. */
+    public Twitter() {
+
+    }
+
+    /** Compose a new tweet. */
+    public void postTweet(int userId, int tweetId) {
+
+    }
+
+    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+    public List<Integer> getNewsFeed(int userId) {
+        return null;
+    }
+
+    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+    public void follow(int followerId, int followeeId) {
+
+    }
+
+    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+    public void unfollow(int followerId, int followeeId) {
+
+    }
+}
--- src/main/深度优先搜索/t529/UpdateBoard.java
+++ src/main/深度优先搜索/t529/UpdateBoard.java
@@ -6,6 +6,17 @@ package 深度优先搜索.t529;
  */
 public class UpdateBoard {
     public char[][] updateBoard(char[][] board, int[] click) {
+        int x = click[0];
+        int y = click[1];
+        return null;
+    }
 
+    private void dfs(char[][] board, int x, int y) {
+        if (board[x][y] == 'M') {
+            board[x][y] = 'X';
+            return ;
+        } else {
+
+        }
     }
 }
