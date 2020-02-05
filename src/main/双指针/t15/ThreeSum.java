e63fdafa40e444bfe4da8653234fe8a723654364
sotowang <sotowang@qq.com>
2020-02-05 15:56:55 +0800

    数学.t367

--- src/main/双指针/t15/ThreeSum.java
+++ src/main/双指针/t15/ThreeSum.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ThreeSum {
-    public static List<List<Integer>> threeSum(int[] nums) {
+    public static List<List<Integer>> threeSum1(int[] nums) {
         ArrayList result = new ArrayList();
         if (nums == null || nums.length < 3) {
             return result;
@@ -48,7 +48,43 @@ public class ThreeSum {
         return result;
     }
 
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList();
+        if(nums == null || nums.length <3){
+            return res;
+        }
+        Arrays.sort(nums);
+        if(nums[0]>0) return res;
+        for(int i=0;i<nums.length-2;i++){
+            if(nums[i]>0){
+                break;
+            }
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum <0) left++;
+                else if(sum == 0){
+                    res.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while(left<right && nums[left]== nums[left+1]){
+                        left++;
+                    }
+                    left++;
+                    right--;
+                }else if(sum>0){
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
+        System.out.println(new ThreeSum().threeSum(new int[]{1, -1, -1, 0}));
         System.out.println(new ThreeSum().threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
         System.out.println(new ThreeSum().threeSum(new int[]{}));
         System.out.println(new ThreeSum().threeSum(new int[]{0, 0, 0}));
---
+++ src/main/数学/t367/IsPerfectSquare.java
@@ -0,0 +1,37 @@
+package 数学.t367;
+
+/**
+ * @auther: sotowang
+ * @date: 2020/2/5 15:12
+ */
+public class IsPerfectSquare {
+    public boolean isPerfectSquare(int num) {
+        if (num == 1) {
+            return true;
+        }
+        long sum;
+        long left = 1;
+        long right = num / 2;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            sum = mid * mid;
+            if (sum > num) {
+                right = mid - 1;
+            } else if (sum < num) {
+                left = mid + 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        assert new IsPerfectSquare().isPerfectSquare(808201) == true;
+        assert new IsPerfectSquare().isPerfectSquare(4) == true;
+        assert new IsPerfectSquare().isPerfectSquare(1) == true;
+        assert new IsPerfectSquare().isPerfectSquare(14) == false;
+        assert new IsPerfectSquare().isPerfectSquare(16) == true;
+    }
+}
