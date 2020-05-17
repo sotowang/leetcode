2781e8c4756feee7d34ed27aa66473c85651eae9
sotowang <sotowang@qq.com>
2020-05-17 22:46:32 +0800

    1019 [链表中的下一个更大节点](https://leetcode-cn.com/problems/next-greater-node-in-linked-list/)

--- README.md
+++ README.md
@@ -805,6 +805,16 @@ r   g  ta  e
 
 * 找出 `A` 中的坡的最大宽度，如果不存在，返回 0 。
 
+## 1019 [链表中的下一个更大节点](https://leetcode-cn.com/problems/next-greater-node-in-linked-list/)
+
+* 给出一个以头节点 `head` 作为第一个节点的链表。链表中的节点分别编号为：`node_1, node_2, node_3, ...` 。
+
+* 每个节点都可能有下一个更大值（*next larger* **value**）：对于 `node_i`，如果其 `next_larger(node_i)` 是 `node_j.val`，那么就有 `j > i` 且 `node_j.val > node_i.val`，而 `j` 是可能的选项中最小的那个。如果不存在这样的 `j`，那么下一个更大值为 `0` 。
+
+* 返回整数答案数组 `answer`，其中 `answer[i] = next_larger(node_{i+1})` 。
+
+* ***注意：***在下面的示例中，诸如 `[2,1,5]` 这样的**输入**（不是输出）是链表的序列化表示，其头节点的值为 2，第二个节点值为 1，第三个节点值为 5 。
+
 ## 1081 [不同字符的最小子序列](https://leetcode-cn.com/problems/smallest-subsequence-of-distinct-characters/)
 
 * 返回字符串 `text` 中按字典序排列最小的子序列，该子序列包含 `text` 中所有不同字符一次。
---
+++ src/main/栈/单调栈/t1019/NextLargerNodes.java
@@ -0,0 +1,28 @@
+package 栈.单调栈.t1019;
+
+/**
+ * @auther: sotowang
+ * @date: 2020/05/17 22:44
+ */
+public class NextLargerNodes {
+    public int[] nextLargerNodes(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int size = 0;
+        ListNode cur = head;
+        while(cur != null){
+            size++;
+            list.add(cur.val);
+            cur = cur.next;
+        }
+        int[] res =  new int[size];
+        LinkedList<Integer> stack = new LinkedList<>();
+        for(int i=size-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()<=list.get(i)){
+                stack.pop();
+            }
+            res[i] = stack.isEmpty()?0:stack.peek();
+            stack.push(list.get(i));
+        }
+        return res;
+    }
+}
