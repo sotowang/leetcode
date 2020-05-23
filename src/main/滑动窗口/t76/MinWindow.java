2ab6e8138c93e409c253e1a4e9bb3c48e979d5b8
sotowang <sotowang@qq.com>
2020-05-23 20:33:36 +0800

    滑动窗口.t76

--- src/main/滑动窗口/t76/MinWindow.java
+++ src/main/滑动窗口/t76/MinWindow.java
@@ -1,57 +1,49 @@
 package 滑动窗口.t76;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @auther: sotowang
  * @date: 2019/11/02 20:03
  */
 public class MinWindow {
     public String minWindow(String s, String t) {
-        if (s == null || s.length() == 0 || s.length() < t.length()) {
+        if(s.isEmpty() || t.isEmpty() || s.length()<t.length()){
             return "";
         }
-        //左右指针
+        int slen = s.length();
+        int tlen = t.length();
+        int[] tnums = new int[100];
+        for(int i=0;i<tlen;i++){
+            tnums[t.charAt(i)-'A']++;
+        }
         int left = 0;
         int right = 0;
-        int start = 0;
-        int minLen = Integer.MAX_VALUE;
-        Map<Character, Integer> windowMap = new HashMap<>();
-        Map<Character, Integer> tMap = new HashMap<>();
-        for (int i = 0; i < t.length(); i++) {
-            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
-        }
-
         int match = 0;
-        while (right < s.length()) {
-            char temChar = s.charAt(right);
-            windowMap.put(temChar, windowMap.getOrDefault(temChar, 0) + 1);
-            if (tMap.containsKey(temChar)) {
-                if (tMap.get(temChar) >= windowMap.get(temChar)) {
+        int start = 0;
+        int min = Integer.MAX_VALUE;
+        int[] window = new int[100];
+        while(right<slen){
+            char temc = s.charAt(right++);
+            window[temc-'A']++;
+            if(tnums[temc-'A']>0){
+                if(window[temc-'A']<=tnums[temc-'A']){
                     match++;
                 }
             }
-            right++;
-            //判断窗口内字符是否满足t中所有字符
-            while (match == t.length()) {
-                //判断是否为目前最小的字符串
-                if (right - left < minLen) {
+            while(match == tlen){
+                if(right-left<min){
+                    min = right-left;
                     start = left;
-                    minLen = right - left;
                 }
-                char tempS = s.charAt(left);
-                windowMap.put(tempS, windowMap.get(tempS) - 1);
-                if (tMap.containsKey(tempS)) {
-                    if (windowMap.get(tempS) < tMap.get(tempS)) {
-                        match--;
-                    }
+                char tem = s.charAt(left);
+                window[tem-'A']--;
+                if(tnums[tem-'A']>0 && tnums[tem-'A']>window[tem-'A']){
+                    match--;
                 }
-                //右移left,缩小窗口
                 left++;
             }
         }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, minLen + start);
+        return min == Integer.MAX_VALUE ? "":s.substring(start,start+min);
+
     }
 
     public static void main(String[] args) {
