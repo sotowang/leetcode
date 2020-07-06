a6ba3464327f4fad14b30a6f622065f9a8e7b66e
sotowang <sotowang@qq.com>
2020-07-06 22:37:20 +0800

    动态规划.t63

--- src/main/动态规划/t63/UniquePathsWithObstacles.java
+++ src/main/动态规划/t63/UniquePathsWithObstacles.java
@@ -6,7 +6,7 @@ package 动态规划.t63;
  * @description:
  */
 public class UniquePathsWithObstacles {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    public int uniquePathsWithObstacles1(int[][] obstacleGrid) {
         int row = obstacleGrid.length;
         int col = obstacleGrid[0].length;
         int[][] path = new int[row][col];
@@ -49,9 +49,47 @@ public class UniquePathsWithObstacles {
         return path[row - 1][col - 1];
     }
 
+    /**
+     * 2020-07-06
+     * @param obstacleGrid
+     * @return
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid == null || obstacleGrid.length ==0 || obstacleGrid[0] == null || obstacleGrid[0].length == 0){
+            return 0;
+        }
+        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        if(obstacleGrid[0][0] == 0){
+            dp[0][0] = 1;
+        }
+        for(int i=1;i<obstacleGrid.length;i++){
+            if(obstacleGrid[i][0] == 0 && obstacleGrid[i-1][0] == 0 && dp[i-1][0] != 0){
+                dp[i][0] = 1;
+            }
+        }
+        for(int i=1;i<obstacleGrid[0].length;i++){
+            if(obstacleGrid[0][i] == 0 && obstacleGrid[0][i-1] == 0 && dp[0][i-1] != 0){
+                dp[0][i] = 1;
+            }
+        }
+        for(int i=1;i<obstacleGrid.length;i++){
+            for(int j=1;j<obstacleGrid[0].length;j++){
+                if(obstacleGrid[i][j] == 0){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                    // System.out.print(dp[i][j] + ",");
+                }
+            }
+        }
+        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1];
+    }
+
     public static void main(String[] args) {
-        System.out.println(new UniquePathsWithObstacles().uniquePathsWithObstacles(new int[][]{{0,1, 0, 0,0}, {1,0,0, 0, 0}, {0, 0, 0,0,0},{0, 0, 0,0,0}}));
         System.out.println(new UniquePathsWithObstacles().uniquePathsWithObstacles(new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
+        System.out.println(new UniquePathsWithObstacles().uniquePathsWithObstacles(new int[][]{
+                {0, 1, 0, 0, 0},
+                {1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0}}));
         System.out.println(new UniquePathsWithObstacles().uniquePathsWithObstacles(new int[][]{{0, 0}}));
         System.out.println(new UniquePathsWithObstacles().uniquePathsWithObstacles(new int[][]{{0, 0}, {1, 1}, {0, 0}}));
     }
