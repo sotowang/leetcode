2111bea0d510f1550ce35256913f3a7619244331
sotowang <sotowang@qq.com>
2020-07-07 22:48:16 +0800

    下一个排列

--- src/main/数组/t31/NextPermutation.java
+++ src/main/数组/t31/NextPermutation.java
@@ -8,45 +8,42 @@ import java.util.Arrays;
  */
 public class NextPermutation {
     public void nextPermutation(int[] nums) {
-        if (nums == null || nums.length <= 1) {
+        if(nums == null || nums.length <= 1){
             return;
         }
-        int firstIndex = -1;
-        for (int i = nums.length - 1; i > 0; i--) {
-            if (nums[i] > nums[i - 1]) {
-                firstIndex = i - 1;
+        //找最大的索引nums[k]<nums[k+1]
+        int k = -1;
+        for(int i=nums.length-1;i> 0;i--){
+            if(nums[i]>nums[i-1]){
+                k = i-1;
                 break;
             }
         }
-        if (firstIndex == -1) {
-            reverse(nums, 0, nums.length - 1);
-        } else {
-            int secondIndex = -1;
-            for (int i = nums.length - 1; i >= 0; i--) {
-                if (nums[i] > nums[firstIndex]) {
-                    secondIndex = i;
-                    break;
-                }
+        //原数组为逆序排序，翻转数组
+        if(k == -1){
+            reverse(nums,0,nums.length-1);
+            return;
+        }
+        //找最大索引 nums[m]>nums[k]
+        int m = -1;
+        for(int i=nums.length-1;i>k;i--){
+            if(nums[i]>nums[k]){
+                m = i;
+                break;
             }
-            swap(nums, firstIndex, secondIndex);
-            reverse(nums, firstIndex + 1, nums.length - 1);
+        }
+        swap(nums,k,m);
+        reverse(nums,k+1,nums.length-1);
+    }
+    private void reverse(int[] nums,int start,int end){
+        while(start<end){
+            swap(nums,start++,end--);
         }
     }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        }
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
+    private void swap(int[] nums,int i,int j){
+        int tem = nums[i];
         nums[i] = nums[j];
-        nums[j] = temp;
+        nums[j] = tem;
     }
 
     public static void main(String[] args) {
