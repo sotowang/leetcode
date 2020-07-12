83e81c04cebfe1bd9a6cc3e99293331862ffa9f5
sotowang <sotowang@qq.com>
2020-07-12 23:47:04 +0800

    扑克问题

--- src/main/mysql/所有科目都大于80的学生/语文及格而数学不及格的同学的学号.sql
+++ src/main/mysql/所有科目都大于80的学生/语文及格而数学不及格的同学的学号.sql
@@ -1,6 +1,6 @@
 select  distinct name from grade.grade
 where name not in
-(select name from grade where course = '数学' and score >=60 )
+(select name from grade where course = '数学' and ifnull(score,0) >=60 )
 and name not in
-(select  name from grade where course = '语文' and score <60)
+(select  name from grade where course = '语文' and ifnull(score,100) <60)
  ;
