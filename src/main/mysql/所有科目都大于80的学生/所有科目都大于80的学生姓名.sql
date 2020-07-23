101b0e6c6e4b90340d39f99a1abf42c3b2640ad7
sotowang <sotowang@qq.com>
2020-07-23 14:21:12 +0800

    动态规划.t64

--- src/main/mysql/所有科目都大于80的学生/所有科目都大于80的学生姓名.sql
+++ src/main/mysql/所有科目都大于80的学生/所有科目都大于80的学生姓名.sql
@@ -1,5 +1,5 @@
 # 查询所有科目都大于80的学生
-select name as 姓名 from grade group by name having min(score)>=80;
+select name as 姓名 from grade group by 姓名 having min(score)>=60;
 
 select distinct name as 姓名 from grade where
 name not in(select name from grade where score<80);
--- src/main/动态规划/t64/MinPathSum.java
+++ src/main/动态规划/t64/MinPathSum.java
@@ -6,44 +6,24 @@ package 动态规划.t64;
  * @description:
  */
 public class MinPathSum {
-    public int minPathSum1(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        for (int i = 0; i < col; i++) {
-            for (int j = 0; j < row; j++) {
-                if (i == 0 && j == 0) {
-                    continue;
-                }
-                if (i == 0) {
-                    grid[j][0] += grid[j - 1][0];
-                    continue;
-                }
-                if (j == 0) {
-                    grid[0][i] += grid[0][i - 1];
-                    continue;
-                }
-                grid[j][i] += Math.min(grid[j - 1][i], grid[j][i - 1]);
-            }
-        }
-        return grid[row - 1][col - 1];
-    }
     public int minPathSum(int[][] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (i == 0 && j == 0) {
-                    continue;
-                } else {
-                    if (i == 0) {
-                        grid[i][j] += grid[i][j - 1];
-                    } else if (j == 0) {
-                        grid[i][j] += grid[i - 1][j];
-                    } else {
-                        grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
-                    }
-                }
+        if(grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0){
+            return 0;
+        }
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for(int i=1;i<rows;i++){
+            grid[i][0] += grid[i-1][0];
+        }
+        for(int i=1;i<cols;i++){
+            grid[0][i] += grid[0][i-1];
+        }
+        for(int i=1;i<rows;i++){
+            for(int j=1;j<cols;j++){
+                grid[i][j] += Math.min(grid[i-1][j],grid[i][j-1]);
             }
         }
-        return grid[grid.length - 1][grid[0].length - 1];
+        return grid[rows-1][cols-1];
     }
 
     public static void main(String[] args) {
