74270f5f3367975847e63165af066f10904e7a75
sotowang <sotowang@qq.com>
2020-07-24 11:55:40 +0800

    位运算.t1025

---
+++ src/main/位运算/t1025/DivisorGame.java
@@ -0,0 +1,11 @@
+package 位运算.t1025;
+
+/**
+ * @auther: sotowang
+ * @date: 2020/07/24 11:53
+ */
+public class DivisorGame {
+    public boolean divisorGame(int N) {
+        return (N&1) != 1;
+    }
+}
