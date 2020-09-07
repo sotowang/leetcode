6bbb4ffe73c534370cc4dfe996b3e533456bf7da
e <sotowang@qq.com>
2020-09-07 21:12:13 +0800

    t347

--- src/main/堆/t347/TopKFrequent.java
+++ src/main/堆/t347/TopKFrequent.java
@@ -7,34 +7,28 @@ import java.util.*;
  * @date: 2020/1/4 20:53
  */
 public class TopKFrequent {
-    public List<Integer> topKFrequent(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || k > nums.length) {
-            return null;
-        }
-        HashMap<Integer, Integer> map = new HashMap<>();
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
-        Collections.sort(entryList,(a,b)->(b.getValue()-a.getValue()));
 
-        //小根堆求最大值
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> map= new HashMap<>();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k,(a,b)->(map.get(a)-map.get(b)));
 
-        for (int i = 0; i < k; i++) {
-            minHeap.offer(entryList.get(i).getKey());
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+
         }
-        for (int i = k; i < entryList.size(); i++) {
-            //检查是否比堆顶元素小
-            //若比堆顶小，丢掉
-            //若比堆顶大，弹出堆顶
-            if (map.get(minHeap.peek()) < entryList.get(i).getValue()) {
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            if(minHeap.size() < k){
+                minHeap.offer(entry.getKey());
+            }else if(map.get(entry.getKey())>map.get(minHeap.peek())){
                 minHeap.poll();
-                minHeap.offer(entryList.get(i).getKey());
+                minHeap.offer(entry.getKey());
             }
         }
-        minHeap.stream().forEach(e -> res.add(e));
+        int[] res = new int[k];
+        int index = 0;
+        while(!minHeap.isEmpty()){
+            res[index++] = minHeap.poll();
+        }
         return res;
     }
 
