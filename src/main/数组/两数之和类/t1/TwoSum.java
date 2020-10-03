2c2b826226209d652102c3abfabe21668c04d2c2
sotowang <sotowang@qq.com>
2020-10-03 16:58:19 +0800

    数组.两数之和类.t1;

--- src/main/数组/两数之和类/t1/TwoSum.java
+++ src/main/数组/两数之和类/t1/TwoSum.java
@@ -10,16 +10,17 @@ import java.util.Map;
  */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                return new int[]{i, map.get(target - nums[i])};
+        int[] res = new int[2];
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                res[0] = map.get(target-nums[i]);
+                res[1] = i;
+                return res;
             }
-            if (!map.containsKey(nums[i])) {
-                map.put(nums[i], i);
-            }
+            map.put(nums[i],i);
         }
-        return new int[]{};
+        return res;
     }
 
     public static void main(String[] args) {
