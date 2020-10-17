f4804eb5d8e976064ef5bd1ac2e90e97ee9187b5
sotowang <sotowang@qq.com>
2020-10-17 19:32:08 +0800

    回溯算法.t52

--- src/main/回溯算法/t52/TotalNQueens.java
+++ src/main/回溯算法/t52/TotalNQueens.java
@@ -1,47 +1,43 @@
 package 回溯算法.t52;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @auther: sotowang
  * @date: 2019/12/24 15:22
  */
 public class TotalNQueens {
     private int count = 0;
+
     public int totalNQueens(int n) {
-        int[][] nums = new int[n][n];
-        //主对角线状态
-        int[] master = new int[n + n - 1];
-        //副对角线状态
-        int[] slave = new int[n + n - 1];
-        int[] column = new int[n];
-        backTrace(nums, 0, 0, master, slave, column);
+        boolean[] master = new boolean[n * 2 - 1];
+        boolean[] slave = new boolean[n * 2 - 1];
+        boolean[] cols = new boolean[n];
+        backTrace(0, 0, master, slave, cols, n);
         return count;
     }
 
-    private void backTrace(int[][] nums, int rowIndex, int colIndex, int[] master, int[] slave, int[] column) {
-        if (colIndex == nums[0].length) {
+    private void backTrace(int row, int col, boolean[] master, boolean[] slave, boolean[] cols, int n) {
+        if (col == n) {
             return;
         }
-        if (rowIndex == nums.length) {
+        if (row == n) {
             count++;
             return;
         }
-        for (int i = 0; i < nums[0].length; i++) {
-            //主对角线判定,次对角线判定,列判定
-            if (master[i - rowIndex + nums.length - 1] == 1 || slave[i + rowIndex] == 1 || column[i] == 1) {
+        //按列放
+        for (int i = 0; i < n; i++) {
+            if (master[i - row + n - 1] || slave[row + i] || cols[i]) {
                 continue;
             }
-            column[i] = 1;
-            slave[i + rowIndex] = 1;
-            master[i - rowIndex + nums.length - 1] = 1;
-            backTrace(nums, rowIndex + 1, i, master, slave, column);
-            column[i] = 0;
-            slave[i + rowIndex] = 0;
-            master[i - rowIndex + nums.length - 1] = 0;
+            master[i - row + n - 1] = true;
+            slave[row + i] = true;
+            cols[i] = true;
+            backTrace(row + 1, i, master, slave, cols, n);
+            master[i - row + n - 1] = false;
+            slave[row + i] = false;
+            cols[i] = false;
         }
     }
+
 
     public static void main(String[] args) {
         System.out.println(new TotalNQueens().totalNQueens(4));
